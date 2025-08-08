76993cb53ed73dd4baa9ed505a7e6cb70784a9b5
Notralles <kronoszl@hotmail.com>
2025-08-08 14:57:26 +0300

    UPDATE

--- Görüntü İşleme/PROJE7/7-CONTOURS.py
+++ Görüntü İşleme/PROJE7/7-CONTOURS.py
@@ -11,14 +11,17 @@ import random
 
 
 output_dir = "D:/ornekresim/konturlar"
-random_thresh = random.randint(0, 255)
+random_thresh = 90     #TAM OLARAK BULAMADIĞI İÇİN HSV LİYE GEÇTİM 
 
 img = cv2.imread("D:/ornekresim/tennisball.jpg") #GÖRÜNTÜYÜ OKUMA
+cv2.imshow("ORIGINAL", img)
+
 gri_top = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #GRAYSCALE
 _, thresh_img = cv2.threshold(gri_top, random_thresh, 255, cv2.THRESH_BINARY)#THRESHOLD
 
 konturlar,_ = cv2.findContours(thresh_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 cv2.drawContours(img, konturlar, -1, (0,255,0),2)
+
 
 cv2.imshow("THRESHOLD {random_thresh}", thresh_img)
 cv2.imshow("GRAYSCALE",gri_top)
--- Görüntü İşleme/PROJE8/8-MASKECONTOURHSV.py
+++ Görüntü İşleme/PROJE8/8-MASKECONTOURHSV.py
@@ -11,12 +11,12 @@ import numpy as np
 
 
 
-
+imgorg = cv2.imread("D:/ornekresim/tennisball.jpg")
 img = cv2.imread("D:/ornekresim/tennisball.jpg")
 hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV) #hsv ye çevirme hsv = hue saturation value
 
 lower = np.array([20,100,100])
-upper = np.array([30,255,255]) #opencv 180 e göre 2 ye bölüyoruz değerleri
+upper = np.array([70,255,255]) #opencv 180 e göre 2 ye bölüyoruz değerleri
 mask = cv2.inRange(hsv, lower, upper)
 #res = cv2.bitwise_and(img, img, mask=mask)
 
@@ -27,15 +27,27 @@ save_path = "D:/ornekresim/konturlar1"
 
 for i, contour in enumerate(contours):
     x, y, w, h = cv2.boundingRect(contour)
-    crop = img[y:y+h, x:x+w]
+
+    boyut = max(w, h)
+    x_centered = x + w // 2 - boyut // 2
+    y_centered = y + h // 2 - boyut // 2
+
+    x_start = max(0, x_centered)
+    y_start = max(0, y_centered)
+    x_end = min(img.shape[1], x_start + boyut)
+    y_end = min(img.shape[0], y_start + boyut)
+
+
+    cv2.rectangle(img, (x_start, y_start), (x_end, y_end), (0, 255, 0), 2)
+
+    
+    crop = img[y_start:y_end, x_start:x_end]
     filename = os.path.join(save_path, f"contour_{i+1}.jpg")
     cv2.imwrite(filename, crop)
     print(f"{filename} kaydedildi.")
     
-    
-    
-    
-cv2.imshow("resim",img) #ORİJİNAL RESİM
+cv2.imshow("orjinal", imgorg)
+cv2.imshow("konturlu",img) #KONTURLU
 cv2.imshow("hsv",hsv) #HSV RENKLER FORMATI RESİ
 cv2.imshow("mask", mask)
 #cv2.imshow("res",res)
